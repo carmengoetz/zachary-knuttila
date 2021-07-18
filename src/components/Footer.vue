4809ac372d26bed75edbf2f2d728c6122f07363a
Carmen Goetz <carmen.b.goetz@gmail.com>
2021-07-17 20:12:23 -0600

    General structure

---
+++ src/components/Footer.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-footer padless class="footer pt-3">
+    <v-col class="text-center" cols="12">
+      <v-row justify="center">
+        <v-btn
+          v-for="social in socials"
+          :key="social.name"
+          class="mx-1"
+          color="black"
+          icon
+          :href="social.link"
+          target="_blank"
+        >
+          <v-icon size="24">
+            {{ social.icon }}
+          </v-icon>
+        </v-btn>
+      </v-row>
+      <v-row>
+        <v-card-text class="black--text">
+          {{ new Date().getFullYear() }} — <strong>Zachary Knuttila</strong>
+        </v-card-text>
+      </v-row>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data: () => {
+    return {
+      socials: [
+        {
+          link: "https://www.twitch.tv/creativereyne",
+          name: "Twitch",
+          icon: "mdi-twitch",
+        },
+        {
+          link: "https://www.youtube.com/channel/UCyHVoqh4TK6XTRIi6m6d6UQ",
+          name: "YouTube",
+          icon: "mdi-youtube",
+        },
+        {
+          link: "https://www.instagram.com/creativereyne/",
+          name: "Instagram",
+          icon: "mdi-instagram",
+        },
+        {
+          link: "https://twitter.com/CreativeReyne",
+          name: "Twitter",
+          icon: "mdi-twitter",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~vuetify/src/styles/styles.sass";
+
+.footer {
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-row no-gutters justify="center" class="mt-16"
+      ><router-link
+        :to="{
+          name: 'Home',
+        }"
+      >
+        <v-card-title> Zachary Knuttila </v-card-title>
+      </router-link></v-row
+    >
+    <v-row no-gutters justify="center" class="mt-8">
+      <router-link
+        v-for="page in pages"
+        class="px-6"
+        :key="page.index"
+        :to="{
+          name: page.name,
+        }"
+      >
+        {{ page.name }}
+      </router-link>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data: () => {
+    return {
+      pages: [
+        { path: "/portfolio", name: "Portfolio", exact: false },
+        { path: "/cv", name: "CurriculumVitae", exact: false },
+      ],
+    };
+  },
+};
+</script>
